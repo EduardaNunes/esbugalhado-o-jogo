c32ffdcd491c0b4bebba577b0cc0d1e9ee973bdf
Duda Nunes <eduardapmn@gmail.com>
2023-12-04 02:26:06 -0300

    Adicionada pontuação dos jogadores e de cada coluna + adicionada algumas decorações no css

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const colunaJogador = document.querySelectorAll('.section-bottom > .coluna')
+const colunaLamb = document.querySelectorAll('.section-bottom > .coluna')
 const colunaRataun = document.querySelectorAll('.section-top > .coluna')
 const lamb = document.querySelector('img#lamb')
 const rataun = document.querySelector('img#rataun')
-const lambDice = document.querySelector('.lamb-dice-container')
+const lambDice = document.querySelector('.lamb-dice-container>p')
 const rataunDice = document.querySelector('.rataun-dice-container')
+const lambPontos = document.querySelector('#pontos-lamb');
+const rataunPontos = document.querySelector("#pontos-rataun");
 
 // Adiciona um eventListener para saber quando o jogador clicar em uma coluna
-for(let i = 0; i<colunaJogador.length; i++){
-    colunaJogador[i].addEventListener('click', escolherColuna)
+for(let i = 0; i<colunaLamb.length; i++){
+    colunaLamb[i].addEventListener('click', escolherColuna)
 }
 
 let vezJogador = true
@@ -31,15 +33,26 @@ function RandomizaDado(){
 
 function escolherColuna(){
 
+    let multiplicador = 1;
+
     // Se for a vez do jogador, ele verifica se existe espaço vazio na coluna escolhida
     if(vezJogador){
 
         for(let i = 0; i < 3; i++){
+            // Verifica se existe algum numero na coluna escolhida que seja igual ao numero tirado no dado
+            
+            if(this.children[i].hasChildNodes() && this.children[i].children[0].innerHTML == numero){
+                multiplicador++
+            }
             if(this.children[i].hasChildNodes() == false){
 
                 // Adiciona o valor na coluna escolhida
                 this.children[i].innerHTML = '<p>' + numero + '</p>'
                 lambDice.innerHTML = ''
+
+                // Chama a função que vai calcular a nova pontuação
+                pontuar(multiplicador, vezJogador, this)
+
                 vezJogador = false
 
                 // Ativa a animação de jogar o dado
@@ -65,15 +78,24 @@ function rataunJoga(){
     // verifica se existe local disponível na coluna
     while(temEspaço == false){
 
+        let multiplicador = 1;
+
         // randomiza a coluna em que o Rataun vai jogar
         let coluna = (Math.floor(Math.random()*3))
     
         for(let i = 0; i < 3; i++){
+
+            if(colunaRataun[coluna].children[i].hasChildNodes() && colunaRataun[coluna].children[i].children[0].innerHTML == numero){
+                multiplicador++
+            }
             if(colunaRataun[coluna].children[i].hasChildNodes() == false){
     
                 // Adiciona o valor na coluna escolhida
                 colunaRataun[coluna].children[i].innerHTML = '<p>' + numero + '</p>'
                 rataunDice.innerHTML = ''
+
+                // Chama a função que vai calcular a nova pontuação
+                pontuar(multiplicador, vezJogador, colunaRataun[coluna])               
                 vezJogador = true
                 temEspaço = true
     
@@ -91,4 +113,28 @@ function rataunJoga(){
     
     // Randomiza o Próximo Dado
     RandomizaDado()
+}
+
+
+function pontuar(multiplicador, vezJogador, coluna){
+
+    let pontosColuna = parseInt(coluna.children[3].innerHTML)
+    let totalPontosLamb = parseInt(lambPontos.innerHTML)
+    let totalPontosRataun = parseInt(rataunPontos.innerHTML)
+
+    // Atualiza a pontuação da coluna selecionada
+    pontosColuna += numero * multiplicador * multiplicador - numero * (multiplicador - 1) * (multiplicador - 1)
+    coluna.children[3].innerHTML = pontosColuna
+
+    // Verifica de quem é a vez e atualiza a pontuação total
+    if(vezJogador){
+        totalPontosLamb += numero * multiplicador * multiplicador - numero * (multiplicador - 1) * (multiplicador - 1)
+        lambPontos.innerHTML = totalPontosLamb
+        
+
+    }else{
+        totalPontosRataun += numero * multiplicador * multiplicador - numero * (multiplicador - 1) * (multiplicador - 1)
+        rataunPontos.innerHTML = totalPontosRataun
+        
+    }
 }
